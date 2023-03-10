d78e4f4ef379ea0d6932dc5cd5c515eecb28256c
mohanjagabattula <mohan.jagabattula@inmar.com>
2023-03-10 11:16:41 +0530

    a wizard with dynamic form fields

---
+++ src/App.tsx
@@ -0,0 +1,82 @@
+import './App.css'
+import Wizard, { Item, FormField } from './wizard';
+import { Form, Input, FormInstance  } from 'antd';
+
+const inputTypeOptions: string[] = ['string', 'number', 'boolean', 'method', 'regexp', 'integer', 'float', 'object', 'enum', 'date', 'url', 'hex', 'email'];
+
+interface DynamicCompProps {
+  antForm: FormInstance
+}
+
+function prepareDynamicFormComponents(): React.FC<DynamicCompProps>[] {
+  const forms = [
+    {title: "Contact Details", fields: [{field: "First Name", type: "text"}, {field: "Email", type: "email"}]},
+    {title: "Agreement", fields: [{field: "Parent Compnay", type: "text"}]},
+    {title: "Feedback", fields: [{field: "Feedback", type: "text"}]}
+  ]
+
+  function prepareDynamicComponent(item: Item): any {
+    /**
+     * Prepares a dynamic component to render form fields
+     * 
+     * with basic form validations based on field's type
+     */
+    return ({antForm}: DynamicCompProps) => {
+      return <>
+        <Form title={item.title} form={antForm}>
+          <h2>{item.title}</h2>
+          {item.fields?.map((field: FormField, fieldIndex: number) => {
+            const rule: any = {
+              ...(inputTypeOptions.includes(field.type) && {type: field.type}),
+              message: `Please enter valid value for ${field.field}`,
+            }
+            return <Form.Item key={fieldIndex} name={field.field} label={field.field} rules={[rule]}>
+                <Input type={field.type}/>
+            </Form.Item>
+          })}
+        </Form>
+        </>
+      }
+  }
+  return forms.map((form: any, index: number) => {
+    return prepareDynamicComponent(form);
+  });
+}
+
+const PackagesList: React.FC<any> = () => {
+  return <div><h2>--Packages--</h2></div>
+}
+
+const Summary: React.FC<any> = () => {
+  return <div><h2>--Summary--</h2></div>
+}
+
+function App() {
+  const [form] = Form.useForm();
+  const dynamicFormComponents = prepareDynamicFormComponents();
+  async function handleNext() {
+    try{
+      const values = await form.validateFields();
+      console.log(values);
+      return true;
+    } catch {
+      console.log("Please check form field values");
+      return false;
+    }
+  }
+  const formSections = dynamicFormComponents.map((Component: React.FC<DynamicCompProps>) => {
+    return {title: "Event Forms", content: <Component antForm={form}/>}
+  })
+  const sections = [
+    {title: "Packages", content: <PackagesList/>},
+    ...formSections,
+    {title: "Summary", content: <Summary/>}
+  ]
+  return (
+    <div className="App">
+      <Wizard sections={sections} onNext={handleNext}/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/wizard.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Button } from 'antd';
+
+export interface FormField {
+    field: string;
+    type: string;
+}
+
+export interface Item {
+    component?: React.FC;
+    title: string;
+    description: string;
+    fields?: FormField[];
+}
+
+export interface WizardProps {
+    items: Item[];
+}
+
+const Wizard: React.FC<any> = ({sections, onNext}) => {
+    const [currentStep, setCurrentStep] = useState(0);
+
+    const handleNext = async () => {
+        try {
+            const status = await onNext();
+            !!status && setCurrentStep(currentStep+1);
+        } catch {
+            console.log("someting went wrong! check console logs.");
+        }
+    }
+
+    return (
+        <div>
+            <Button disabled={currentStep === 0} onClick={() => setCurrentStep(currentStep-1)}>Previous</Button>
+            <Button disabled={currentStep === sections.length-1} onClick={handleNext}>Next</Button>
+            {sections[currentStep].content}
+        </div>
+    )
+}
+
+export default Wizard;
